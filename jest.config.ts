422b55225321b0855b0c837e4220dfca741a3101
Tamas Varga <tamas.varga.420@gmail.com>
2024-04-21 22:17:26 +0200

    chore: add jest + rtl + test packages

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import nextJest from "next/jest.js";
+
+import type { Config } from "jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+};
+
+export default createJestConfig(config);
